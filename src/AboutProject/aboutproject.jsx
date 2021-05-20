23d8100365abc4ebb5821c6f0ec61ed309ca1fc0
Marta Banas <banas.m.marta@gmail.com>
2021-05-20 21:38:22 +0200

    opraveno about page + nastylovano homepage

--- src/AboutProject/aboutproject.jsx
+++ src/AboutProject/aboutproject.jsx
@@ -1,30 +1,32 @@
 import React from 'react';
 import './style.css';
-import './img/zbenice01.jpg';
 
 const AboutProject = () => {
   return (
     <>
       <section className="project">
-        <h1>O projektu</h1>
+        <h1 className="project__title">Zámek Zbenice</h1>
+        <div className="project__box--photo">
+          <img className="project__photo" src="./img/zbenice01.jpg" alt="Zamek Zbenice" loading="lazy" />
+        </div>
+        <div className="project__info">
+          <p className="project__description">
+            Zbenice jsou zámek ve stejnojmenné vesnici na jihu okresu Příbram ve
+            Středočeském kraji. Jeho předchůdcem byla starší středověká tvrz,
+            kterou nechal v roce 1626 Vilém Šléglovský ze Šicendorfu přestavět na
+            renesanční zámek, později upravený v barokním slohu. Zámecký areál se
+            nachází na severozápadním okraji vesnice a je chráněn jako kulturní
+            památka.
+          </p>
 
-        <p></p>
+          <p className="project__description">
+            Současný majitel si zámek koupil před 15 lety a nyní se snaží postupně
+            zámek rekonstruovat a vrátit ho do doby ze 16. století.
+          </p>
 
-        <h3>Zámek Zbenice</h3>
-        <img className="photo" src="./img/zbenice01.jpg" alt="Zamek Zbenice" />
-        <p>
-          Zbenice jsou zámek ve stejnojmenné vesnici na jihu okresu Příbram ve
-          Středočeském kraji. Jeho předchůdcem byla starší středověká tvrz,
-          kterou nechal v roce 1626 Vilém Šléglovský ze Šicendorfu přestavět na
-          renesanční zámek, později upravený v barokním slohu. Zámecký areál se
-          nachází na severozápadním okraji vesnice a je chráněn jako kulturní
-          památka.
-        </p>
-
-        <p>
-          Současný majitel si zámek koupil před 15 lety a nyní se snaží postupně
-          zámek rekonstruovat a vrátit ho do bodoby ze 16. století.
-        </p>
+          <p className="project__description">Našim cílem je vytvořit moderní webové stránky zámku Zbenice, které budou lákat navštěvníky z celé České republiky.</p>
+        </div>
+        
       </section>
     </>
   );
--- src/Home/home.jsx
+++ src/Home/home.jsx
@@ -2,7 +2,16 @@ import React from 'react';
 import './style.css';
 
 const Home = () => {
-  return <p>Jsem domovská stránka</p>;
+  return (
+    <>
+      <section className="home">
+      <h1 className="home__title">
+        Vítejte na stránce věnované závěrečnému projektu Digitální Akademie Web jaro 2021. Akademii pořáda nezísková organizace Czechitas, která podporuje mimo jiné ženy v IT.
+      </h1>
+      <img src="./img/Czechitas_logo.png" className="home__logo" alt="logo Czechitas" />
+      </section>
+    </>
+  );
 };
 
 export default Home;
